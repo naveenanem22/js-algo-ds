a7e8ed42f1dcfe279e603da51ae29630018bd45c
Naveen Kumar Anem <naveenanem22@gmail.com>
2022-02-21 22:37:44 +0530

    DP fibonacci series tests

--- dynamic-programming/FibonacciSeries.js
+++ dynamic-programming/FibonacciSeries.js
@@ -14,3 +14,28 @@ export function getFibonacciNumbers(n) {
   }
   return series;
 }
+
+export function getNthFibonacciNumberByDP(n) {
+  const fibonacciMap = new Map();
+  if (!fibonacciMap.has(n)) {
+    if (n === 0) {
+      fibonacciMap.set(n, 0);
+    } else if (n === 1) {
+      fibonacciMap.set(n, 1);
+    } else {
+      fibonacciMap.set(
+        n,
+        getNthFibonacciNumber(n - 1) + getNthFibonacciNumber(n - 2)
+      );
+    }
+  }
+  return fibonacciMap.get(n);
+}
+
+export function getFibonacciNumbersByDP(n) {
+  const series = [];
+  for (let i = 0; i <= n; i++) {
+    series.push(getNthFibonacciNumberByDP(i));
+  }
+  return series;
+}
--- dynamic-programming/__tests__/FibonacciSeries.test.js
+++ dynamic-programming/__tests__/FibonacciSeries.test.js
@@ -1,4 +1,9 @@
-import { getNthFibonacciNumber, getFibonacciNumbers } from "../FibonacciSeries";
+import {
+  getNthFibonacciNumber,
+  getFibonacciNumbers,
+  getFibonacciNumbersByDP,
+  getNthFibonacciNumberByDP,
+} from "../FibonacciSeries";
 import { test, expect } from "@jest/globals";
 
 test("Nth fibonacci number test", () => {
@@ -17,4 +22,24 @@ test("Get first N fibonacci numbers", () => {
   expect(getFibonacciNumbers(n)).toEqual([0, 1, 1, 2, 3, 5]);
   n = 10;
   expect(getFibonacciNumbers(n)).toEqual([0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55]);
+});
+
+test("Nth fibonacci number test by using Dynamic Programming", () => {
+  let n = 0;
+  expect(getNthFibonacciNumberByDP(n)).toEqual(0);
+  n = 14;
+  expect(getNthFibonacciNumberByDP(n)).toEqual(377);
+  n = 13;
+  expect(getNthFibonacciNumberByDP(n)).toEqual(233);
+  n = 5;
+  expect(getNthFibonacciNumberByDP(n)).toEqual(5);
+});
+
+test("Get first N fibonacci number by using Dynamic Programming", () => {
+  let n = 5;
+  expect(getFibonacciNumbersByDP(n)).toEqual([0, 1, 1, 2, 3, 5]);
+  n = 10;
+  expect(getFibonacciNumbersByDP(n)).toEqual([
+    0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55,
+  ]);
 });
